55b5723c577f6ecd9eb12125461ef25be8acf365
Chuck <firstinmath2@gmail.com>
2025-08-21 23:05:29 -0400

    addedvercel

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import Taskbar from "@/components/Taskbar";
 import "./globals.css";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
